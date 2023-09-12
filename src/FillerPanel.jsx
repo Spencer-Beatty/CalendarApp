67a40f4b64379cc6369cfd5f3186e9152565ee7c
Spencer-Beatty <spencer.j.beatty@gmail.com>
2023-09-12 19:45:55 -0400

    completed filler panel

--- src/FillerPanel.jsx
+++ src/FillerPanel.jsx
@@ -1,3 +1,36 @@
+import TimeSelect from "./TimeSelect"
+import "./fillerPanel.css"
 export default function FillerPanel(props){
-    return(<></>)
+    return(
+        <div className="filler-panel">
+            <div className="new-filler-event">
+                New Filler Event
+                <form className="new-filler-event-form">
+                    Category
+                    <select className="new-filler-event-form-category-select">
+                        {props.categories.map(category => {
+                            return(<option className="new-filler-event-form-category-select-option" value={category.type}>{category.type}</option>)
+                        })}
+                    </select>
+                    Title
+                    <input type="text" className="new-filler-event-form-title"></input>
+                    Duration
+                    <select className="new-filler-event-form-duration">
+                        <option >1</option>
+                    </select>
+                    <button className="new-filler-event-form-submit-btn">Submit</button>
+                </form>
+
+
+            </div>
+            <div className="view-filler-event">
+                <button className="view-filler-event-btn"> View Filler Events </button>
+            </div>
+
+
+        </div>
+    
+    
+    
+        )
 }
